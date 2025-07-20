d8d1c3a146e43fdb2dbfecc4c8b4262c832594f2
Roxana Teodora <catana.roxana97@gmail.com>
2025-07-20 20:16:38 +0300

    fix api error from server.js

--- src/2.pages/SearchPage/petApiService.js
+++ src/2.pages/SearchPage/petApiService.js
@@ -2,19 +2,27 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const petApi = createApi({
   reducerPath: "petApi",
-  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:5000/api/pets" }),
+  baseQuery: fetchBaseQuery({
+    baseUrl: import.meta.env.VITE_API_URL,
+  }),
   endpoints: (builder) => ({
     getPet: builder.query({
-      query: (id) => `pets?id=${id}`, // Actualizat pentru a utiliza endpoint-ul corect
-      transformResponse: (response) => response.pets[0], // Extrage primul animal din răspuns
+      query: (id) => ({
+        url: "/pets",
+        params: { id },
+      }),
+      transformResponse: (response) => response.pets?.[0], // Extrage primul animal
     }),
     getBreeds: builder.query({
-      query: (animal) => `breeds?animal=${animal}`,
+      query: (animal) => ({
+        url: "/breeds",
+        params: { animal },
+      }),
       transformResponse: (response) => response.breeds,
     }),
     search: builder.query({
       query: ({ animal, location, breed }) => ({
-        url: "pets",
+        url: "/pets",
         params: { animal, location, breed },
       }),
       transformResponse: (response) => response.pets,
