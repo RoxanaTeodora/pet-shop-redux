563e4df937a1e26e852341078d4f0e97e435d264
Roxana Teodora <catana.roxana97@gmail.com>
2025-07-21 19:41:34 +0300

    fix vercel error of caps name in UserCreate

--- src/2.pages/AdminPage/UserCreate.jsx
+++ src/2.pages/AdminPage/UserCreate.jsx
@@ -1,7 +1,7 @@
 // import React from 'react'
 import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { addUser } from "./userReducer";
+import { addUser } from "./UserReducer";
 import { useNavigate } from "react-router-dom";
 
 function CreateUsers() {
